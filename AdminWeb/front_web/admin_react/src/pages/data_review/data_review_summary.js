75df059f45a7a19d27bb2649a23dc091c4762edd
ZhiYi Wang <wjq20011219@gmail.com>
2023-10-11 17:49:18 +1100

    Add front end data

--- AdminWeb/front_web/admin_react/src/pages/data_review/data_review_summary.js
+++ AdminWeb/front_web/admin_react/src/pages/data_review/data_review_summary.js
@@ -2,29 +2,44 @@
 // Admin and manager can view data summary in this page
 
 // Import library
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Import CSS
 import styles from '../../stylesheets/data_review.module.css'
 
 const DataReviewSummary = () =>{
 
-    //  // Data Variable
-    //  const [data, setData] =  useState([
-    //     { question_id: 'Story', question_first: 'Text', topic: '12345@gmail.com', type: 'unimelb',  },
-    //     { question_id: 'Wellbeing', question_first: 'Rating', topic: '23456@gmail.com', group: 'unimelb'},
-    //     { question_id: 'Story', question_first: 'Drop down', topic: '34567@gmail.com', group: 'unimelb' },
-    //     { question_id: 'Employability', question_first: 'Text', topic: '45678@gmail.com', group: 'Volunteering Victoria' },
-    //     { question_id: 'Wellbeing', question_first: 'Number', email: '56789@gmail.com', group: 'Volunteering Victoria' },
-    //     { question_id: 'Story', question_first: 'Rating', email: '67890@gmail.com', group: 'Volunteering Victoria' },
-    //     { question_id: 'Story', question_first: 'Text', email: 'abcde@gmail.com', group: 'Volunteering Victoria' },
-    //     { question_id: 'Wellbeing', question_first: 'Rating', email: 'bcdef@gmail.com', group: 'unimelb' },
-    //     { question_id: 'Story', question_first: 'Drop down', email: 'cdefg@gmail.com', group: 'unimelb' },
-    //     { question_id: 'Employability', question_first: 'Text', email: 'defga@gmail.com', group: 'unimelb' },
-    //     { question_id: 'Wellbeing', question_first: 'Number', email: 'efgab@gmail.com', group: 'unimelb' },
-    //     { question_id: 'Story', question_first: 'Rating', email: 'fgabc@gmail.com', group: 'Volunteer West' },
-    //     { question_id: 'Story', question_first: 'Text', email: 'gabcd@gmail.com', group: 'Volunteer West' },
-    // ]);
+     // Data Variable
+     const [data, setData] =  useState([
+        { question_id: '1', question_first: 'What was your role?', topic: 'Story', rate_min: '', rate_max: '', type: 'Text', text_one: 'Nursing', text_two:'Nursing', number:'', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '1', question_first: 'What was your role?', topic: 'Story', rate_min: '', rate_max: '', type: 'Text', text_one: 'Tutor', text_two:'Tutor', number:'', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '1', question_first: 'What was your role?', topic: 'Story', rate_min: '', rate_max: '', type: 'Text', text_one: 'Nursing', text_two:'Nursing', number:'', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '2', question_first: 'How many hours do you volunteer?', topic: 'Story', rate_min: '', rate_max: '', type: 'Number', text_one: '', text_two:'', number:'35', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '2', question_first: 'How many hours do you volunteer?', topic: 'Story', rate_min: '', rate_max: '', type: 'Number', text_one: '', text_two:'', number:'24', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '2', question_first: 'How many hours do you volunteer?', topic: 'Story', rate_min: '', rate_max: '', type: 'Number', text_one: '', text_two:'', number:'18', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '3', question_first: 'How would you rate your wellbeing status?', topic: 'Wellbeing', rate_min: 'Terrible', rate_max: 'Fantastic', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'1', yes_or_no:'', dropdown_text:'' },
+        { question_id: '3', question_first: 'How would you rate your wellbeing status?', topic: 'Wellbeing', rate_min: 'Terrible', rate_max: 'Fantastic', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'0', yes_or_no:'', dropdown_text:'' },
+        { question_id: '3', question_first: 'How would you rate your wellbeing status?', topic: 'Wellbeing', rate_min: 'Terrible', rate_max: 'Fantastic', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'2', yes_or_no:'', dropdown_text:'' },
+        { question_id: '4', question_first: 'Are you looking to change your work in some ways at some point?', topic: 'Employability', rate_min: '', rate_max: '', type: 'Y/N', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'true', dropdown_text:'' },
+        { question_id: '4', question_first: 'Are you looking to change your work in some ways at some point?', topic: 'Employability', rate_min: 'Fantastic', rate_max: '', type: 'Y/N', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'true', dropdown_text:'' },
+        { question_id: '3', question_first: 'How would you rate your wellbeing status?', topic: 'Wellbeing', rate_min: 'Terrible', rate_max: '', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'-2', yes_or_no:'', dropdown_text:'' },
+        { question_id: '1', question_first: 'What was your role?', topic: 'Story', rate_min: '', rate_max: '', type: 'Text', text_one: 'Supervious', text_two:'Supervious', number:'', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '1', question_first: 'What was your role?', topic: 'Story', rate_min: '', rate_max: '', type: 'Text', text_one: 'Vet', text_two:'Nursing', number:'', rating:'', yes_or_no:'', dropdown_text:'' },
+        { question_id: '4', question_first: 'Are you looking to change your work in some ways at some point?', topic: 'Employability', rate_min: '', rate_max: '', type: 'Y/N', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'false', dropdown_text:'' },
+        { question_id: '4', question_first: 'Are you looking to change your work in some ways at some point?', topic: 'Employability', rate_min: '', rate_max: '', type: 'Y/N', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'true', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'3', yes_or_no:'', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'0', yes_or_no:'', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'3', yes_or_no:'', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'-1', yes_or_no:'', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'-1', yes_or_no:'', dropdown_text:'' },
+        { question_id: '5', question_first: 'How often do you practice 5 ways of wellbeing? - Connect', topic: 'Wellbeing', rate_min: 'Never', rate_max: 'Always', type: 'Rating scales 1-5', text_one: '', text_two:'', number:'', rating:'-1', yes_or_no:'', dropdown_text:'' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'Chinese' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'Chinese' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'Chinese' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'English' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'Chinese' },
+        { question_id: '6', question_first: 'What lanague do you spoke at home?', topic: 'Demographic Questions', rate_min: '', rate_max: '', type: 'dropdown', text_one: '', text_two:'', number:'', rating:'', yes_or_no:'', dropdown_text:'English' },
+    ]);
 
     return (
         <div id={styles.data_review_summary}>
