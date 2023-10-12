5bfa2ab28090a26c92a38fc62901345c052b03ee
xinshi1 <xinshi1@student.unimelb.edu.au>
2023-10-12 16:40:35 +1100

    add question type dropdown layout

--- WeConnectApp/front_app/app/build.gradle.kts
+++ WeConnectApp/front_app/app/build.gradle.kts
@@ -52,6 +52,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //View pdf
+    implementation ("com.github.barteksc:android-pdf-viewer:3.2.0")
+
+
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.20")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.20")
--- WeConnectApp/front_app/app/src/main/java/com/example/weconnectapp/ui/privacyPolice/PrivacyPoliceFragment.java
+++ WeConnectApp/front_app/app/src/main/java/com/example/weconnectapp/ui/privacyPolice/PrivacyPoliceFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import android.text.Html;
+
 
 import com.example.weconnectapp.databinding.FragmentPrivacyPoliceBinding;
 
